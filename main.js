b1303563251e369d3b697df63008ef7c723503a1
marcoj18 <marcojosegoparmulbacher1234@gmail.com>
2023-01-24 10:08:13 +0000

    Add numID

--- main.js
+++ main.js
@@ -4,32 +4,50 @@ const generate = document.getElementById('generate');
 const md = document.getElementById('md');
 const textarea = document.getElementById('textarea');
 
-let createFile = '';
+let createFile = ''; // Here add the all content of the file
+let count = 0; // Count num of img 
+let varLoop = []; // Count the content of the document
+let idNum = [];
+
+//This function create a title
 
 const crearTitulo = ()=>{
     let titulo = prompt('Titulo oficial');
     createFile += '# ' + titulo + '\n';
 };
 
+//This function create a id for every link 
+
 const numId = text =>{
-    let replace = text.replace(/#|_/g,'');
-    let num = replace.slice(1,4);
-    let withOutSpace = num.replace(' ','');
+    for (let i of text) {
+        let wihtOutSharp = i.replace(/#|_/g,'');
+        let num = wihtOutSharp.slice(1,4);
+        let withOutSpace = num.replace(' ','');
+        idNum.push(Number(withOutSpace));
+        
+    }
+
 }
 
+
+//This function create a list in markdown format
 
 const crearLista = text =>{
-   let addHTML = `- <a href='id${withOutSpace}'>${replace}</a>`;
-   createFile += addHTML + '\n';
+   //let addHTML = `- <a href='id${withOutSpace}'>${replace}</a>`;
+   //createFile += addHTML + '\n';
 }
+
+//This function format the header of markdown files
 
 const formatMD = text =>{
-    createFile += `${text} <a name="id"></a>` + '\n';
+    //console.log(`${text} <a name="id"></a>`);
+    //createFile += `${text} <a name="id"></a>` + '\n';  
+    
 }
 
 
 
-let count = 0;
+// This function create a file named README.md whit all content
 
 
 const downloadFile = () =>{
@@ -44,18 +62,25 @@ const downloadFile = () =>{
     });
 };
 
+//This is called where initial all 
+
 readFile.addEventListener('change',()=>{
-    crearTitulo();
+    //crearTitulo();
     const reader = new FileReader();
     reader.addEventListener('load',(e)=>{
         let result = e.target.result;
         for (let i of result.split('\n')){
             if(i.startsWith('#') && !i.includes('#!/bin/sh') && !i.includes('#!/bin/bash')){
-                formatMD(i);
+                varLoop.push(i);
                 
             }
         }
+        numId(varLoop);
+        crearLista(varLoop);
+        formatMD(varLoop);
     })
     reader.readAsText(readFile.files[0]);
     downloadFile();
 })
+
+
